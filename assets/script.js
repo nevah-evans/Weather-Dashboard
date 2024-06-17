4c4dc2182e619b8a0787ec18834575b23510d754
nevah_evans <nevahevans@yahoo.com>
2024-06-16 19:48:48 -0500

    adding base design and placeholder data

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,33 @@
 const APIKey = "84ae5461ca0dd25d6c96a7e3edea8d1c";
+const searchBtn = document.getElementById('search');
+
+searchBtn.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let city = document.getElementById("search").value;
+    localStorage.setItem("city", city);
+})
+
+// localStorage.getItem('city');
+
+
+// const geoCode =`http://api.openweathermap.org/geo/1.0/direct?q=${city},{state code},{US}&limit=5&appid=${APIKey}`;
+
+// const querURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+// fetch(geoCode)
+// .then(response => {
+//     if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+// })
+
+// fetch(querURL)
+// .then(response => {
+//     if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+// })
+

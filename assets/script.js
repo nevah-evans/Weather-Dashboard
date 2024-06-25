8b7b46768df609b740e132968bf21031549c6b66
nevah_evans <nevahevans@yahoo.com>
2024-06-25 00:02:00 -0500

    adding JS to display current weather and five day forecast in sync with valid data.

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,124 @@
 const APIKey = "84ae5461ca0dd25d6c96a7e3edea8d1c";
-const searchBtn = document.getElementById('search');
+const searchBtn = document.getElementById('submit');
 
-searchBtn.addEventListener('submit', function(event){
+
+searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
 
-    let city = document.getElementById("search").value;
-    localStorage.setItem("city", city);
-})
-
-// localStorage.getItem('city');
+    // renderSearchHistory();
+    renderWeather();
+});
 
 
-// const geoCode =`http://api.openweathermap.org/geo/1.0/direct?q=${city},{state code},{US}&limit=5&appid=${APIKey}`;
 
-// const querURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+function renderWeather() {
 
-// fetch(geoCode)
-// .then(response => {
-//     if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-// })
+    const cityName = document.getElementById("search").value;
 
-// fetch(querURL)
-// .then(response => {
-//     if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-// })
+    let lon = localStorage.getItem('longitude');
+    let lat = localStorage.getItem('latitude');
 
+const fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
+fetch(fiveDay)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // console.log(data.city.name);
+        // console.log(data.list[0].main.temp);
+
+
+        const now = dayjs();
+        document.getElementById('current-date').innerHTML = now.format('MM/DD/YYYY');
+
+        for (let i = 0; i < 5; i++) {
+            document.getElementById('date' + (i + 1)).innerHTML = now.add(i + 1, 'd').format('MM/DD/YYYY');
+        }
+        const fiveDayForecast = [];
+        const fiveForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!fiveDayForecast.includes(forecastDate)) {
+                return fiveDayForecast.push(forecastDate);
+            }
+        });
+
+        console.log(fiveForecast);
+
+        for (let i = 0; i < fiveForecast.length; i++) {
+            document.getElementById('temperature' + (i + 1)).innerHTML = `Temp: ${(fiveForecast[i].main.temp).toFixed(0)} °F`;
+            document.getElementById('hum' + (i + 1)).innerHTML = `Humidity: ${fiveForecast[i].main.humidity} %`;
+            document.getElementById('windy' + (i + 1)).innerHTML = `Wind: ${(fiveForecast[i].wind.speed).toFixed(0)} mph`;
+            document.getElementById('icon' + (i + 1)).src = ` https://openweathermap.org/img/wn/${fiveForecast[i].weather[0].icon}.png`;
+        }
+    })
+
+
+    const querURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
+
+
+    fetch(querURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+
+            // console.log(data);
+            // console.log(data.name);
+            // console.log(data.coord.lon);
+            // console.log(data.coord.lat);
+            // console.log(data.main.temp);
+            // console.log(data.wind.speed);
+            // console.log(data.main.humidity);
+
+            localStorage.setItem('city', data.name);
+            localStorage.setItem('longitude', data.coord.lon);
+            localStorage.setItem('latitude', data.coord.lat);
+
+            let icon = data.weather[0].icon;
+            const weatherIcon = `https://openweathermap.org/img/wn/${icon}.png`;
+
+            document.querySelector('#city-name').innerHTML = data.name;
+            document.querySelector('#icon').src = weatherIcon;
+
+            document.querySelector('#temp').innerHTML = 'Temp: ' + (data.main.temp).toFixed(0) + '°F';
+            document.querySelector('#humidity').innerHTML = 'Humidity: ' + data.main.humidity + ' %';
+            document.querySelector('#wind').innerHTML = 'Wind: ' + (data.wind.speed).toFixed(0) + ' mph';
+
+        })
+
+
+
+};
+
+
+// const searchHistory = document.querySelector('.search-history')
+
+// function renderSearchHistory(){
+// const history = localStorage.getItem('city');
+
+// // searchHistory.innerHTML = "";
+
+// for (let i =0; i< search.length; i++){
+//     // const search = searches[i];
+
+//     // const li = document.createElement('li');
+//     // li.textContent = history;
+//     // li.setAttribute('history', i);
+
+//     // search.append(li);
+//     let search= document.getElementById('history').innerHTML = history;
+//     searchHistory.append(search);
+// }
+
+// };
+
+// function init(){
+//     renderSearchHistory();
+// }
+// init();
